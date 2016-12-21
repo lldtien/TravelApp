c524b2e6c546d543c34a3c145a6c78ec8a387d0d
vungho <hoquocvung@gmail.com>
2016-12-21 09:58:39 +0700

    Sửa phần MainActivity

--- app/src/main/java/com/travelapp/MainActivity.java
+++ app/src/main/java/com/travelapp/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("ahihi", "Tít bướm");
-        Log.i("ahihi", "Tít bướm");
     }
 }
